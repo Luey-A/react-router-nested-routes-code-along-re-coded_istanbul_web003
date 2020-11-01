e8643ac9b980a642d072f7bf431ea3c3aa9c56de
Loae Monla Ahmed <lueyahmad@gmail.com>
2020-11-01 13:10:36 +0000

    Automatically backed up by Learn

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,10 +1,7 @@
-import React, { Component } from 'react';
-import {
-  BrowserRouter as Router,
-  Route
-} from 'react-router-dom';
-import NavBar from '../components/NavBar';
-import MoviesPage from './MoviesPage';
+import React, { Component } from "react";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import NavBar from "../components/NavBar";
+import MoviesPage from "./MoviesPage";
 
 class App extends Component {
 
